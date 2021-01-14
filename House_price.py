011ac5e32d15e2a2c14401ff979ae249778a4de6
Permpoon Boonyarit <76562714+dotZQ@users.noreply.github.com>
2021-01-14 16:30:46 +0700

    Update House_price.py

--- House_price.py
+++ House_price.py
@@ -11,6 +11,7 @@ df.corr(method='pearson').style.format("{:.2}").background_gradient(cmap=plt.get
 
 num = 60
 s =[]
+#this loop find the best random_state
 for i in range(num):
   train , test = train_test_split(df,test_size = 0.2 , random_state = i)
   X = train[['sqft_living','grade']]
@@ -25,6 +26,7 @@ for i in range(num):
   this_score = regr.score(Xtest,Ytest)
   s.append(this_score)
 
+#indicate the best randomstate to f
 d = max(s)
 f = s.index(d)
 
@@ -42,5 +44,6 @@ regr.fit(X,Y)
 score = regr.score(Xtest,Ytest)
 print("score : ",score)
 
+#you can try by change the first parameter to sqft_living and the second one to grade of your house
 price = regr.predict([[1370,7]])
 print("predicted_price : ", int(price))
